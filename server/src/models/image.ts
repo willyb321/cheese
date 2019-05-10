4d7035c50544d32bb42b7e87a9011cf19272da68
willyb321 <wbwilliam7@gmail.com>
2019-05-11 07:18:06 +1000

    img model improvements

--- server/src/models/image.ts
+++ server/src/models/image.ts
@@ -1,4 +1,5 @@
 'use strict';
+const shortid = require('shortid');
 
 module.exports = (sequelize, DataType) => {
   return sequelize.define(
@@ -9,18 +10,25 @@ module.exports = (sequelize, DataType) => {
         autoIncrement: true,
         primaryKey: true
       },
+      shortId: {
+        type: DataType.STRING,
+        defaultValue: shortid.generate
+      },
       originalName: {
         type: DataType.STRING
       },
       fileName: {
         type: DataType.STRING
       },
-      userId: {
+      fileHash: {
         type: DataType.STRING
       },
       fileExt: {
         type: DataType.STRING
-      }
+      },
+      userId: {
+        type: DataType.STRING
+      },
     },
     {
       indexes: [
